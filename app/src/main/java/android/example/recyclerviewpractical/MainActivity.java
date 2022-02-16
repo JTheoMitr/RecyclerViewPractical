475fc44678e77d37acd397d847d1d6a7f188f0f0
John <jmitropoulos@alumni.berklee.edu>
2022-02-15 21:01:19 -0600

    Initial commit

---
+++ app/src/main/java/android/example/recyclerviewpractical/MainActivity.java
@@ -0,0 +1,20 @@
+package android.example.recyclerviewpractical;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter programAdapter;
+    RecyclerView.LayoutManager layoutManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
